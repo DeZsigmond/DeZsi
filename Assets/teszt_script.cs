c73d5c1332467930a14231dec21c56b995e5f8d5
VinczerGabor <zsigmond.keri@gmail.com>
2023-09-26 21:00:58 +0200

    Initial commit

---
+++ Assets/teszt_script.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+public class teszt_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HELLO UNITY!");
+    }
+
+
+}
