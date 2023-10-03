56563fb9b5242f608748382365352029d29fec44
VinczerGabor <zsigmond.keri@gmail.com>
2023-10-03 20:59:48 +0200

    2.

---
+++ Assets/scriptek/ControlScaters.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+class ControlStructures : MonoBehaviour
+{
+    [SerializeField] int number;
+
+    void Start()
+    {
+
+        if (number >= 10)
+        {
+            Debug.Log("Többjegyû szám");
+        }
+        else
+        {
+            Debug.Log("Egyjegyû szám");
+        }
+
+        string isEven;
+        if (number % 2 == 0)
+            isEven = "EVEN";
+        isEven = number % 2 == 0 ? "EVEN" : "odd";
+
+        int I = 1;
+        while (i <= 100)
+            Debug.Log(i * i);
+        I++;
+        for (int j = 1; j <= 100; j++)
+            Debug.Log(j * j);
+
+
+        int k = 1;
+        do
+        {
+            Debug.Log(k * k);
+            while (k <= 100) ;
+        }
+
+
+
+    }
+}
