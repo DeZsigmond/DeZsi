9942acf6ad94346a0dc8de7a46ecf41b6fbc445f
Gabor <gabor.vinczer@proton.me>
2023-11-21 21:08:02 +0100

    7.

--- Assets/scriptek/AutoRotate.cs
+++ Assets/scriptek/AutoRotate.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 class AutoRotator : MonoBehaviour
 {
     [SerializeField] float angularSpeed = 360; //Szögsebesség
+    [SerializeField] Space space = Space.Self;
 
     void Update()
     {
-        transform.Rotate(0, angularSpeed * Time.deltaTime, 0);
+        transform.Rotate(0, angularSpeed * Time.deltaTime, 0, space);
     }
 }
